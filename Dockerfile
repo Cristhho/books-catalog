841b3c5bd4a54246642b469be81021249af28371
christian <cristhho@gmail.com>
2024-02-10 18:31:19 -0500

    vite env config

--- Dockerfile
+++ Dockerfile
@@ -14,13 +14,8 @@ WORKDIR /app
 COPY --from=deps /app/node_modules ./node_modules
 COPY . .
 
-# Next.js collects completely anonymous telemetry data about general usage.
-# Learn more here: https://nextjs.org/telemetry
-# Uncomment the following line in case you want to disable telemetry during the build.
+EXPOSE ${VITE_PORT}
 
-EXPOSE 3000
-
-ENV PORT 3000
 ENV WATCHPACK_POLLING true
 
 CMD [ "npm", "run", "dev" ]
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    }
-  },
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+  const env = {...loadEnv(mode, process.cwd(), 'VITE_')};
+  
+  return {
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      }
+    },
+    server: {
+      port: Number(env.VITE_PORT)
+    },
+    plugins: [react()],
+  }
 })
